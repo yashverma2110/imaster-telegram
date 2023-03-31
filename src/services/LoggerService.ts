e7b769814d2b4cae5bf3ee1f4a5fc0a0eba4f0b8
yashverma2110 <yash.silver2110@gmail.com>
2023-03-31 16:19:59 +0530

    fix(logger): added colour resetting

--- src/services/LoggerService.ts
+++ src/services/LoggerService.ts
@@ -1,13 +1,19 @@
+const Reset = '\x1b[0m';
 const BgGreen = '\x1b[42m';
 const BgRed = '\x1b[41m';
+const BgYellow = '\x1b[43m';
 
 const LoggerService = {
   success(...args: any) {
-    console.log(BgGreen, ...args);
+    console.log(BgGreen, ...args, Reset);
+  },
+
+  info(...args: any) {
+    console.log(BgYellow, ...args, Reset);
   },
 
   error(...args: any) {
-    console.log(BgRed, ...args);
+    console.log(BgRed, ...args, Reset);
   },
 };
 
